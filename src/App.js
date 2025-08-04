71fc84e8cf7ed4e693963a3ecd08604976e612ec
haroldiaz <harolfz@gmail.com>
2025-08-03 22:35:44 -0500

    chore: solucion rutas router-dom:

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 
 import '../src/Styles/App.css';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistroReporte from './Pages/RegistroReporte';
 import VerReporteProduccion from './Pages/VerReporteProduccion';
 
 function App() {
   return (
      <div className="App">
-      <BrowserRouter>
-      
       <Router>
         <Routes>
           <Route path="/" element={<RegistroReporte />} />
           <Route path="/verReporte" element={<VerReporteProduccion />} />
         </Routes>
       </Router>
-    </BrowserRouter>
     </div>
   );
 }
--- src/Components/supabaseClient.js
+++ src/Components/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-console.log('URL:', process.env.REACT_APP_SUPABASE_URL);
-console.log('KEY:', process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
