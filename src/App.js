183fb5c91bbede31ecb50ad97fbdad62ad773243
haroldiaz <harolfz@gmail.com>
2025-08-03 22:32:21 -0500

    feat: add routes a paginas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 
 import '../src/Styles/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import RegistroReporte from './Pages/RegistroReporte';
 import VerReporteProduccion from './Pages/VerReporteProduccion';
 
 function App() {
   return (
      <div className="App">
+      <BrowserRouter>
+      
       <Router>
         <Routes>
           <Route path="/" element={<RegistroReporte />} />
-          <Route path="/VerReporte" element={<VerReporteProduccion />} />
+          <Route path="/verReporte" element={<VerReporteProduccion />} />
         </Routes>
       </Router>
+    </BrowserRouter>
     </div>
   );
 }
